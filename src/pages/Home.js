0b141b66e73223c41a89baeb61569568d5a8641f
KamaljeetSingh <kamaljitsinghbadhan1@gmail.com>
2021-07-05 00:03:24 +0530

    added layout management & created title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
   return (
-    <div>
+    <MainPageLayout>
       <h1>This is Home Page</h1>
-    </div>
+    </MainPageLayout>
   );
 };
 
--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 const Starred = () => {
   return (
-    <div>
+    <MainPageLayout>
       <h1>This is Starred Page</h1>
-    </div>
+    </MainPageLayout>
   );
 };
 
