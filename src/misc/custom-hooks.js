31cce0b56e96579cf3f37bde4fbc8a1f0e4a9e55
KamaljeetSingh <kamaljitsinghbadhan1@gmail.com>
2021-07-06 16:53:32 +0530

    used custom hooks for starred

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -3,7 +3,7 @@ import { useReducer, useEffect } from 'react';
 function showReducer(prevState, action) {
   switch (action.type) {
     case 'ADD': {
-      return [...prevState, action.type];
+      return [...prevState, action.showId];
     }
     case 'REMOVE': {
       return prevState.filter(showId => showId !== action.showId);
@@ -26,6 +26,6 @@ function usePersistedReducer(reducer, initialState, key) {
   return [state, dispatch];
 }
 
-function useShows(key = 'shows') {
+export function useShows(key = 'shows') {
   return usePersistedReducer(showReducer, [], key);
 }
